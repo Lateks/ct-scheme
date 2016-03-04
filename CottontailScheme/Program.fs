eb8c3a456b50e0bdb6de60583d63cd2f3ccfdc89
Laura Leppänen <laura.leppanen@iki.fi>
2016-03-04 16:47:05 +0200

    Initial commit, testing FParsec

---
+++ CottontailScheme/Program.fs
@@ -0,0 +1,26 @@
+﻿open FParsec
+
+open FParsec
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+test pfloat "1.25"
+
+test pfloat "1.25E 3"
+
+let str s = pstring s
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+
+let floatBetweenBrackets = pfloat |> betweenStrings "[" "]"
+let floatBetweenDoubleBrackets = pfloat |> betweenStrings "[[" "]]"
+
+test floatBetweenBrackets "[1.0]"
+test floatBetweenBrackets "[]"
+test floatBetweenBrackets "[1.0"
+test floatBetweenDoubleBrackets "[[9.2]]"
+
+
+System.Console.ReadKey() |> ignore
---
+++ CottontailScheme/Test.fsx
@@ -0,0 +1,24 @@
+﻿#r "..\\packages\\FParsec.1.0.2\\lib\\net40-client\\FParsecCS.dll"
+#r "..\\packages\\FParsec.1.0.2\\lib\\net40-client\\FParsec.dll"
+
+open FParsec
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+test pfloat "1.25"
+
+test pfloat "1.25E 3"
+
+let str s = pstring s
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+
+let floatBetweenBrackets = pfloat |> betweenStrings "[" "]"
+let floatBetweenDoubleBrackets = pfloat |> betweenStrings "[[" "]]"
+
+test floatBetweenBrackets "[1.0]"
+test floatBetweenBrackets "[]"
+test floatBetweenBrackets "[1.0"
+test floatBetweenDoubleBrackets "[[9.2]]"
