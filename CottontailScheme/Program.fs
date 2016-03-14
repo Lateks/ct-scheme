48de7108fc05435480ee025f0f6cc77e24b73b59
Laura Leppänen <laura.leppanen@iki.fi>
2016-03-14 19:34:39 +0200

    Added a separate test project and tested boolean parsing.

--- CottontailScheme/Program.fs
+++ CottontailScheme/Program.fs
@@ -1,120 +1,12 @@
-﻿open FParsec
+﻿open CottontailScheme.Parsing
+open FParsec
 
-type CTDatum = CTBool of bool
-             | CTNumber of float
-             | CTString of string
-             | CTSymbol of string
-             | CTList of CTDatum list
-
-type CTIdentifier = CTIdentifier of string
-
-type CTLambdaExpressionFormals = CTFormalsSingle of CTIdentifier
-                               | CTFormalsList of CTIdentifier list
-
-type CTExpression = CTIdentifierExpression of CTIdentifier
-                  | CTLiteralExpression of CTDatum
-                  | CTProcedureCallExpression of CTExpression * CTExpression list
-                  | CTLambdaExpression of CTLambdaExpressionFormals * CTDefinition list * CTExpression list
-                  | CTConditionalExpression of CTExpression * CTExpression * CTExpression option
-                  | CTAssignmentExpression of CTIdentifier * CTExpression
-
-and CTDefinition = CTDefinition of CTIdentifier * CTExpression
-
-type CTTopLevelCommand = CTTopLevelDefinition of CTDefinition
-                       | CTTopLevelExpression of CTExpression
-
-type CTProgram = CTProgram of CTTopLevelCommand list
-
-let test p str =
+let testParser p str =
     match run p str with
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
-let str s = pstring s
-
-let parseBoolean = skipChar '#' >>. (((pstring "true" <|> pstring "t") >>% CTBool true)
-                                <|> ((pstring "false" <|> pstring "f") >>% CTBool false))
-
-test parseBoolean "#true"
-test parseBoolean "#f"
-test parseBoolean "#a"
-
-// TODO: big numbers?
-// TODO: parse integers separately?
-let parseNumber =
-    let numberFormat =     NumberLiteralOptions.AllowMinusSign
-                       ||| NumberLiteralOptions.AllowPlusSign
-                       ||| NumberLiteralOptions.AllowFraction
-    numberLiteral numberFormat "number"
-    |>> fun nl -> CTNumber (float nl.String)
-
-test parseNumber "3.14159"
-test parseNumber "+5"
-test parseNumber "-42"
-
-let parseStringLiteral =
-    let escape = anyOf "\"\\tnr"
-                 |>> function
-                     | 't' -> "\t"
-                     | 'n' -> "\n"
-                     | 'r' -> "\r"
-                     |  c  -> string c
-    let escapedChar = pstring "\\" >>. escape
-    let normalCharSeq = manySatisfy (fun c -> c <> '"' && c <> '\\')
-    between (pstring "\"") (pstring "\"")
-            (stringsSepBy normalCharSeq escapedChar)
-    |>> CTString
-
-test parseStringLiteral "\"\""
-test parseStringLiteral "\"foobar\""
-test parseStringLiteral "\"lol\\\\bal\\n\\r\\t\\\"foo\\\"\""
-
-// This parser currently accepts a subset of the
-// identifiers specified by the R7RS grammar.
-let parseSymbolOrIdentifier =
-    let isSpecialChar = isAnyOf "!$%&*/:<=>?^_~"
-    let isExplicitSign = isAnyOf "+-"
-    let isInitialChar c = isAsciiLetter c || isSpecialChar c
-    let isSubsequentChar c = isInitialChar c || isDigit c || isExplicitSign c
-    let isSignSubsequent c = isInitialChar c || isExplicitSign c
-    let regularIdentifierFormat = IdentifierOptions(isAsciiIdStart = isInitialChar,
-                                                    isAsciiIdContinue = isSubsequentChar)
-    let parseRegularIdentifier = identifier regularIdentifierFormat
-    let parsePeculiarIdentifier = manyStrings2 (many1Chars2 (satisfy isExplicitSign) (satisfy isSignSubsequent))
-                                               parseRegularIdentifier
-    parseRegularIdentifier <|> parsePeculiarIdentifier
-
-test parseSymbolOrIdentifier "+"
-test parseSymbolOrIdentifier "-"
-test parseSymbolOrIdentifier ">"
-test parseSymbolOrIdentifier ">10"
-test parseSymbolOrIdentifier "fib"
-test parseSymbolOrIdentifier "call-with-current-continuation"
-test parseSymbolOrIdentifier "+$30"
-test parseSymbolOrIdentifier "+fib+"
-test parseSymbolOrIdentifier "42" // should return empty string
-test parseSymbolOrIdentifier "+1" // should return +
-
-let parseSymbol = parseSymbolOrIdentifier |>> CTSymbol
-
-let parseDatum, parseDatumRef = createParserForwardedToRef<CTDatum, unit>()
-
-let wspace = spaces
-
-let parseList = between (pstring "(") (pstring ")")
-                        (wspace >>. sepBy parseDatum spaces1 .>> wspace)
-                |>> CTList
-
-do parseDatumRef := choice [parseList
-                            parseBoolean
-                            parseNumber
-                            parseStringLiteral
-                            parseSymbol]
-
-test parseList "(1 2 3)"
-test parseList "((1 2 3) (a b c))"
-test parseDatum "+3.14159"
-test parseDatum "\"Hello, world!\""
-test parseDatum "#true"
-test parseDatum "call-with-current-continuation"
-test parseDatum "+"
+[<EntryPoint>]
+let main args =
+    testParser parseBoolean "#true"
+    0
---
+++ CottontailSchemeTests/ParserTest.fs
@@ -0,0 +1,42 @@
+﻿namespace CottontailSchemeTests
+
+open NUnit.Framework
+open FsUnit
+open FParsec
+
+open CottontailScheme
+open CottontailScheme.Parsing
+
+module ParserTest =
+    let testEquals expected p str =
+        match run p str with
+        | Success(result, _, _) -> result |> should equal expected
+        | Failure(errorMsg, _, _) -> Assert.Fail("Parser gave error: " + errorMsg)
+
+    let testRejects p str =
+        match run p str with
+        | Success(result, _, _) -> Assert.Fail("Erroneously accepted " + str)
+        | Failure(errorMsg, _, _) -> Assert.Pass()
+
+[<TestFixture>]
+type ``Boolean literal parser`` () =
+    [<Test>]
+    member x.``recognizes literal values representing true`` () =
+        let testAccepts = ParserTest.testEquals (CTBool true) parseBoolean
+        testAccepts "#true"
+        testAccepts "#t"
+
+    [<Test>]
+    member x.``recognizes literal values representing false`` () =
+        let testAccepts = ParserTest.testEquals (CTBool false) parseBoolean
+        testAccepts "#false"
+        testAccepts "#f"
+
+    [<Test>]
+    member x.``rejects other values`` () =
+        let testRejects = ParserTest.testRejects parseBoolean
+        testRejects "#False"
+        testRejects "#"
+        testRejects "true"
+        testRejects "42"
+        testRejects ""
