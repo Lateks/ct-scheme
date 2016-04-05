915d8dddca9597a42fe50e3b659ae1fb175674e5
Laura Leppänen <laura.leppanen@iki.fi>
2016-04-05 18:00:36 +0300

    Reading input files

--- CottontailScheme/Program.fs
+++ CottontailScheme/Program.fs
@@ -1,22 +1,30 @@
 ﻿open CottontailScheme.Parsing
 open CottontailScheme.ASTBuilder
+open CottontailScheme.SymbolGenerator
 open CottontailScheme.SemanticAnalysis
 open CottontailScheme.CodeGenerator
 
 open FParsec
+open System
 
 [<EntryPoint>]
 let main args =
-    let programName = "hello"
-    let programCode = ""
+    if args.Length < 1 then
+        printfn "Error: no input file specified"
+    else
+        let inputFileName = IO.Path.GetFileNameWithoutExtension args.[0]
+        let programName = inputFileName.Split([|"."; "_"|], StringSplitOptions.RemoveEmptyEntries)
+                          |> fun xs -> String.Join("-", xs)
+                          |> kebabCaseToCamelCase
+        let programCode = IO.File.ReadAllText(args.[0])
 
-    match run CottontailScheme.Parsing.parseProgram programCode with
-    | Success(result, _, _)
-        -> match buildAST result with
-            | ASTBuildSuccess exprs
-               -> match analyse exprs with
-                  | ValidProgram (exprs, scope) -> generateCodeFor exprs scope programName
-                  | ProgramAnalysisError err -> failwithf "Program analysis failed: %A" err
-            | ASTBuildFailure errs -> failwithf "AST building failed with errors: %A" errs
-    | Failure(errorMsg, _, _) -> failwithf "Parsing failed: %A" errorMsg
+        match run CottontailScheme.Parsing.parseProgram programCode with
+        | Success(result, _, _)
+            -> match buildAST result with
+                | ASTBuildSuccess exprs
+                   -> match analyse exprs with
+                      | ValidProgram (exprs, scope) -> generateCodeFor exprs scope programName
+                      | ProgramAnalysisError err -> printfn "Error: %s" err
+                | ASTBuildFailure errs -> errs |> List.map (fun e -> printfn "Error (line %i, column %i): %A" e.position.line e.position.column e.message) |> ignore
+        | Failure(errorMsg, _, _) -> printfn "Error: %s" errorMsg
     0
