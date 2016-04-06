5548a6407a5e6689fd162f22fa017582d1e35a8d
Laura Leppänen <laura.leppanen@iki.fi>
2016-04-06 11:30:50 +0300

    First implementation: compiling a hello, world program in Scheme

--- CottontailScheme/CodeGenerator.fs
+++ CottontailScheme/CodeGenerator.fs
@@ -2,6 +2,7 @@
 
 open Scope
 open SemanticAnalysis
+open Literals
 
 open CottontailSchemeLib
 
@@ -19,32 +20,59 @@ let copyLibs () =
     let csLib = "CottontailSchemeLib.dll"
     IO.File.Copy(exePath + "/" + csLib, IO.Directory.GetCurrentDirectory() + "/" + csLib)
 
+let emitStringObjectCreation (gen : Emit.ILGenerator) (s : string) =
+    gen.Emit(Emit.OpCodes.Ldstr, s)
+    gen.Emit(Emit.OpCodes.Newobj, typeof<CTString>.GetConstructor([| typeof<string> |]))
+
+let emitBuiltInFunctionCall (gen : Emit.ILGenerator) (id : Identifier) =
+    match id.uniqueName with
+    | "display" -> gen.Emit(Emit.OpCodes.Call, typeof<GenericOperations>.GetMethod("GetDisplayValue"))
+                   gen.Emit(Emit.OpCodes.Call, typeof<Console>.GetMethod("Write", [| typeof<string> |]))
+    | e -> failwithf "Not implemented yet! (built-in function %s)" e
+
+// TODO: handling args differently? (when to pop results off the stack?)
+let rec generateExpression (gen : Emit.ILGenerator) (expr : Expression) (scope : Scope) =
+    match expr with
+    | ProcedureCall (proc, args)
+        -> for arg in args do
+               generateExpression gen arg scope
+           match proc with
+           | VariableReference id -> if List.contains id (getBuiltIns scope) then
+                                        emitBuiltInFunctionCall gen id
+                                     else
+                                        failwithf "Not implemented yet ! (generic procedure calls)"
+           | e -> failwithf "Not implemented yet! %A" e
+    | ValueExpression lit
+        -> match lit with
+           | String s -> emitStringObjectCreation gen s
+           | e -> failwithf "Not implemented yet! %A" e
+    | e -> failwithf "Not implemented yet! %A" e
+
+let generateMainModule (mainClass : Emit.TypeBuilder) (mainMethod : Emit.MethodBuilder) (program : ProgramStructure) =
+    let ilGen = mainMethod.GetILGenerator()
+    for expr in program.expressions do
+        generateExpression ilGen expr program.scope
+
+    ilGen.Emit(Emit.OpCodes.Ldc_I4_0)
+    ilGen.Emit(Emit.OpCodes.Ret)
+
+    mainClass.CreateType()
+
 let generateCodeFor (program : ProgramStructure) (name : string) =
     let capitalizedName = SymbolGenerator.capitalizeWord name
     let outputFileName = sprintf "%s.exe" capitalizedName
     let assemblyBuilder = setupAssembly capitalizedName
     let moduleBuilder = assemblyBuilder.DefineDynamicModule(capitalizedName, outputFileName);
 
-    // Create class
-    let typeBuilder = moduleBuilder.DefineType(capitalizedName, TypeAttributes.Public ||| TypeAttributes.Class)
+    // Create class for the "module body"
+    let mainClass = moduleBuilder.DefineType(capitalizedName, TypeAttributes.Public ||| TypeAttributes.Class)
+    let mainMethod = mainClass.DefineMethod("Main", MethodAttributes.Public ||| MethodAttributes.Static,
+                                            typeof<int>, [| typeof<string array> |])
 
-    // Create main method
-    let methodBuilder = typeBuilder.DefineMethod("Main", MethodAttributes.Public ||| MethodAttributes.Static,
-                                                 typeof<int>, [| typeof<string array> |])
-    let ilGen = methodBuilder.GetILGenerator()
-    // Emit instructions for the main method
-    ilGen.Emit(Emit.OpCodes.Ldsfld, typeof<Constants>.GetField("True"))
-    ilGen.Emit(Emit.OpCodes.Call, typeof<Console>.GetMethod("WriteLine", [| typeof<Object> |]))
-    ilGen.Emit(Emit.OpCodes.Ldstr, "Hello, world!")
-    ilGen.Emit(Emit.OpCodes.Call, typeof<Console>.GetMethod("WriteLine", [| typeof<string> |]))
-    ilGen.Emit(Emit.OpCodes.Ldc_I4_0)
-    ilGen.Emit(Emit.OpCodes.Ret)
-
-    // Finalize generated class
-    let t = typeBuilder.CreateType()
+    generateMainModule mainClass mainMethod program |> ignore
 
     // Save assembly and mark it as a console application
-    assemblyBuilder.SetEntryPoint(methodBuilder, Emit.PEFileKinds.ConsoleApplication)
+    assemblyBuilder.SetEntryPoint(mainMethod, Emit.PEFileKinds.ConsoleApplication)
     assemblyBuilder.Save(outputFileName)
 
     copyLibs()
--- CottontailScheme/Scope.fs
+++ CottontailScheme/Scope.fs
@@ -6,6 +6,15 @@ type Scope = { definitions: Identifier list; parent: Scope option }
 
 let addDefinition scope identifier = { definitions = identifier::scope.definitions; parent = scope.parent}
 
+let getBuiltIns scope =
+    let rec findRootScope scope =
+        match scope.parent with
+        | None -> scope
+        | Some s -> findRootScope s
+
+    let rootScope = findRootScope scope
+    rootScope.definitions
+
 let findProgramScope scope =
     let rec find prev scope =
         match scope.parent with
