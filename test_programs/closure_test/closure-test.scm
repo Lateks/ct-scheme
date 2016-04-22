f1f9bb0edc0fec0fe2bdc5479742cdac4c56748f
Laura Leppänen <laura.leppanen@iki.fi>
2016-04-22 13:16:17 +0300

    Better test case reporting for closure_test.scm

--- test_programs/closure_test/closure-test.scm
+++ test_programs/closure_test/closure-test.scm
@@ -1,14 +1,19 @@
 (define test
-  (lambda (test-name got expected)
-    (display test-name)
-	(display " ... ")
-    (if (eq? got expected)
-	    (display "OK\n")
-		(begin (display "FAILED: expected ")
+  (lambda (test-name test expected)
+	(define perform-test
+	  (lambda ()
+		(define got (test))
+		(if (eq? got expected)
+			(display "OK\n")
+			(begin (display "FAILED: expected ")
 		       (display expected)
 			   (display " but got ")
 			   (display got)
 			   (newline)))))
+    (display test-name)
+	(display " ... ")
+	(perform-test)
+))
 
 (define add
   (lambda (x)
@@ -16,12 +21,12 @@
 	  (+ x y))))
 	  
 (test "argument capture: add 5 10"
-      ((add 5) 10)
+      (lambda () ((add 5) 10))
 	  15)
 
 (define add5 (add 5))
 (test "saved closure: add5 10"
-      (add5 10)
+      (lambda () (add5 10))
 	  15)
 
 (define add4
@@ -32,7 +37,7 @@
 		  (+ x y z t))))))
 
 (test "deep nesting #1: add4"
-      ((((add4 2) 3) 4) 5)
+      (lambda () ((((add4 2) 3) 4) 5))
 	  14)
 
 (define mult4
@@ -45,7 +50,7 @@
 		  (* xyz t))))))
 
 (test "deep nesting #2: mult4"
-      ((((mult4 2) 3) 4) 5)
+      (lambda () ((((mult4 2) 3) 4) 5))
 	  120)
 
 (define global-var 0)
@@ -58,7 +63,7 @@
 
 ((((global-variable-visibility 2) 3) 4) 5)
 (test "deep nesting #3: global-variable-visibility"
-	  global-var
+	  (lambda () global-var)
 	  120)
 
 (define increment-counter #f)
@@ -77,7 +82,7 @@
 
 (setup-counter)
 (test "counter #1: starting value"
-      (get-counter)
+      (lambda () (get-counter))
 	  0)
 
 (increment-counter)
@@ -85,11 +90,11 @@
 (increment-counter)
 
 (test "counter #2: after three increments"
-      (get-counter)
+      (lambda () (get-counter))
 	  3)
 
 (test "counter #3: increment and get"
-      (inc-and-get-counter)
+      (lambda () (inc-and-get-counter))
 	  4)
 
 (define my-plus
@@ -97,7 +102,7 @@
     (+ x y)))
 
 (test "top level closure #1: my-plus"
-      (my-plus 5 6)
+      (lambda () (my-plus 5 6))
 	  11)
 
 (set! my-plus
@@ -105,7 +110,7 @@
     (* x y)))
 
 (test "top level closure #2: my-plus after set!"
-      (my-plus 5 6)
+      (lambda () (my-plus 5 6))
 	  30)
 
 (define counter 0)
@@ -116,17 +121,17 @@
 
 (define increment5 (create-counter-incrementer 5))
 (test "referencing top level names from nested context #1: starting value"
-      counter
+      (lambda () counter)
       0)
 
 (increment5)
 (test "referencing top level names from nested context #2: after incrementing once"
-	  counter
+	  (lambda () counter)
 	  5)
 
 (increment5)
 (test "referencing top level names from nested context #3: after incrementing twice"
-	  counter
+	  (lambda () counter)
 	  10)
 
 (define square (lambda (x) (* x x)))
@@ -140,11 +145,11 @@
 
 (define circle (create-circle 2))
 (test "referencing top level names from nested context #4: circle radius"
-      ((car circle))
+      (lambda () ((car circle)))
 	  2)
 
 (test "referencing top level names from nested context #5: circle area"
-      ((cdr circle))
+      (lambda () ((cdr circle)))
 	  (* pi 4))
 
 (define capture-and-mutate-arg
@@ -156,12 +161,12 @@
 
 (define cpt-and-mutate (capture-and-mutate-arg 5))
 (test "argument capture #1: starting value"
-	  ((car cpt-and-mutate))
+	  (lambda () ((car cpt-and-mutate)))
 	  50)
 
 ((cdr cpt-and-mutate) 42)
 (test "argument capture #2: after setting"
-      ((car cpt-and-mutate))
+      (lambda () ((car cpt-and-mutate)))
 	  42)
 
 (define non-capturing-closure-1
@@ -169,7 +174,7 @@
     (lambda (x) (+ x 1))))
 
 (test "non-capturing closures #1: at first nesting level (no other capturing lambdas in the same scope)"
-      ((non-capturing-closure-1) 5)
+      (lambda () ((non-capturing-closure-1) 5))
 	  6)
 
 (define non-capturing-closure-2
@@ -180,7 +185,7 @@
 
 (define ncc2 (non-capturing-closure-2 99))
 (test "non-capturing closures #2: at first nesting level, capturing lambda in the same scope"
-      (+ ((cdr ncc2)) ((car ncc2)))
+      (lambda () (+ ((cdr ncc2)) ((car ncc2))))
 	  (+ 42 99))
 
 (define local-non-capturing-procedures
@@ -193,10 +198,9 @@
 			-1)))
 	check))
 
-
 (define local-proc-call (local-non-capturing-procedures))
 (test "calling a local procedure #1: named otherwise non-capturing procedures defined at the same nesting level"
-      (local-proc-call)
+      (lambda () (local-proc-call))
 	  1)
 	  
 (define create-counter
@@ -214,7 +218,7 @@
 (counter2)
 (counter2)
 (test "calling a local procedure #2: named capturing procedures defined at the same nesting level"
-      (counter2)
+      (lambda () (counter2))
 	  3)
 
 (define really-complex-way-of-computing-circle-area
@@ -230,7 +234,7 @@
 
 (define circle-area (really-complex-way-of-computing-circle-area))
 (test "calling a local procedure #3: named non-capturing procedures defined at a higher nesting level"
-      (circle-area 5)
+      (lambda () (circle-area 5))
 	  (* (* 5 5) 3.14159))
 
 (define some-kind-of-multiplier-thing
@@ -244,7 +248,7 @@
 
 (define mult-fun (some-kind-of-multiplier-thing 5))
 (test "calling a local procedure #4: named capturing procedures defined at a higher nesting level"
-      (mult-fun 4)
+      (lambda () (mult-fun 4))
 	  40)
 
 (define reverse
@@ -266,7 +270,7 @@
 	(reverse (multiply-list l '()))))
 
 (test "local functional loop"
-      (multiply '(1 2 3 4 5) 3)
+      (lambda () (multiply '(1 2 3 4 5) 3))
 	  '(3 6 9 12 15))
 
 (define make-list
@@ -286,10 +290,10 @@
 (define head (car lista))
 (define last (cdr lista))
 (test "varargs argument capture #1"
-      (head)
+      (lambda () (head))
 	  1)
 (test "varargs argument capture #2"
-      (last)
+      (lambda () (last))
 	  10)
 
 (define add-fc
@@ -300,7 +304,7 @@
 	addx))
 
 (test "first class use of local procedure name #1"
-      ((add-fc 4) 6)
+      (lambda () ((add-fc 4) 6))
 	  10)
 
 (define transform-and-double
@@ -315,7 +319,7 @@
 	(transform-and-double f v1)))
 
 (test "first class use of local procedure name #2"
-	  (transform 6 3)
+	  (lambda () (transform 6 3))
 	  4)
 
 (define double-closure
@@ -329,11 +333,11 @@
 (define second-result ((cdr dc)))
 
 (test "first class use of local procedure name #3"
-      first-result
+      (lambda () first-result)
 	  42)
 
 (test "first class use of local procedure name #4"
-	  second-result
+	  (lambda () second-result)
 	  43)
 
 (define odd?
@@ -349,7 +353,7 @@
 		(odd? (- n 1)))))
 
 (test "mutual recursion between top level procedures"
-      (odd? 9999999)
+      (lambda () (odd? 9999999))
 	  #true)
 
 (define odd-fun? #f)
@@ -368,7 +372,7 @@
 		(odd-fun? (- n 1)))))
 
 (test "mutual recursion between closures created inside the module body (main method)"
-      (odd-fun? 9999999)
+      (lambda () (odd-fun? 9999999))
 	  #true)
 
 (define is-odd
@@ -386,5 +390,5 @@
 	(odd? n even?)))
 
 (test "mutual recursion between local closures passed as first class objects to procedures"
-	(is-odd 9999999)
+	(lambda () (is-odd 9999999))
 	#true)
