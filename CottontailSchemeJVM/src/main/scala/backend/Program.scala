aa87dc0abb466358217cc12ba3ea4fd2d6c8b8b8
Laura Leppänen <laura.leppanen@iki.fi>
2016-04-26 11:26:13 +0300

    Use json4s for parsing JSON from stdin

--- CottontailSchemeJVM/src/main/scala/backend/Program.scala
+++ CottontailSchemeJVM/src/main/scala/backend/Program.scala
@@ -1,7 +1,19 @@
 package backend
 
+import scala.io.Source
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
 object Program {
   def main(args: Array[String]): Unit = {
+    val input = Source.stdin.getLines
+    if (input.isEmpty) {
+      println("No input")
+    } else {
+      val json = parse(input.mkString("\n"))
+      println(json)
+    }
+
     println("Hello from Scala!")
   }
 }
