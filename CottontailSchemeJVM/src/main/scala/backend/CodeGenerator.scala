4e5bd020d2c4f173a7712e6340385fce321b7bea
Laura Leppänen <laura.leppanen@iki.fi>
2016-04-27 17:58:50 +0300

    Switch to ASM and generate a simple Hello, world program as an example again

--- CottontailSchemeJVM/src/main/scala/backend/CodeGenerator.scala
+++ CottontailSchemeJVM/src/main/scala/backend/CodeGenerator.scala
@@ -1,62 +1,31 @@
 package backend
 
-import java.io.{DataOutputStream, FileOutputStream}
-import javassist.bytecode.{AccessFlag, Bytecode, ClassFile, MethodInfo}
+import java.io.FileOutputStream
 
 import backend.ast._
+import backend.codegen.SimpleMethodVisitor
+import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Opcodes._
 
 object CodeGenerator {
 
   def generateCodeFor(program : ProgramSyntaxTree) : Unit = {
-    // Using the higher level API:
-    //val classPool = ClassPool.getDefault
-    //val mainClass = classPool.makeClass(program.programName)
-    //val mainMethod = CtNewMethod.make(
-    //  "public static void main(String[] args) { System.out.println(\"Hello, world!\"); }",
-    //  mainClass)
-    //mainClass.addMethod(mainMethod)
-    //mainClass.writeFile(".")
+    val mainClass = new ClassWriter(0)
+    mainClass.visit(V1_8, ACC_PUBLIC, program.programName, null, "java/lang/Object", new Array[String](0))
+    val mainMethod = new SimpleMethodVisitor(mainClass, ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V")
+    mainMethod.emitGetStatic("java/lang/System", "out", "Ljava/io/PrintStream;")
+    mainMethod.visitLdcInsn("Hello, world!")
+    mainMethod.emitInvokeVirtual("java/io/PrintStream", "println", "(Ljava/lang/String;)V", false)
+    mainMethod.emitReturn()
+    mainMethod.visitEnd()
 
-    // Defining an abstract method and giving it a body later:
-    // CtMethod m = new CtMethod(CtClass.intType, "move",
-    //  new CtClass[] { CtClass.intType }, cc);
-    // cc.addMethod(m);
-    // m.setBody("{ x += $1; }");
-    // cc.setModifiers(cc.getModifiers() & ~Modifier.ABSTRACT);
+    val maxStack = 2
+    val maxLocals = 1
+    mainMethod.visitMaxs(maxStack, maxLocals)
+    mainClass.visitEnd()
 
-    val mainClass = new ClassFile(false, program.programName, "java/lang/Object")
-    mainClass.setAccessFlags(AccessFlag.PUBLIC | AccessFlag.SUPER)
-
-    // Add main method
-    val mainMethod = new MethodInfo(mainClass.getConstPool, "main", "([Ljava/lang/String;)V")
-    mainMethod.setAccessFlags(AccessFlag.PUBLIC | AccessFlag.STATIC)
-
-    val mainMethodBody = new Bytecode(mainClass.getConstPool)
-    mainMethodBody.addGetstatic("java/lang/System", "out", "Ljava/io/PrintStream;")
-    mainMethodBody.addLdc("Hello, world!")
-    mainMethodBody.addInvokevirtual("java/io/PrintStream", "println", "(Ljava/lang/Object;)V")
-    mainMethodBody.addReturn(null)
-    mainMethodBody.setMaxLocals(1)
-
-    mainMethod.setCodeAttribute(mainMethodBody.toCodeAttribute)
-
-    mainClass.addMethod(mainMethod)
-
-    // Add constructor
-    val initializer = new MethodInfo(mainClass.getConstPool, MethodInfo.nameInit, "()V")
-    initializer.setAccessFlags(AccessFlag.PUBLIC)
-
-    val initializerBody = new Bytecode(mainClass.getConstPool)
-    initializerBody.addAload(0)
-    initializerBody.addInvokespecial("java/lang/Object", MethodInfo.nameInit, "()V")
-    initializerBody.addReturn(null)
-    initializerBody.setMaxLocals(1)
-
-    initializer.setCodeAttribute(initializerBody.toCodeAttribute)
-
-    mainClass.addMethod(initializer)
-
-    mainClass.write(new DataOutputStream(new FileOutputStream(program.programName + ".class")))
+    val bytes = mainClass.toByteArray
+    new FileOutputStream(program.programName + ".class").write(bytes)
   }
 
 }
---
+++ CottontailSchemeJVM/src/main/scala/backend/codegen/SimpleMethodVisitor.scala
@@ -0,0 +1,120 @@
+package backend.codegen
+
+import org.objectweb.asm._
+import org.objectweb.asm.Opcodes._
+
+class SimpleMethodVisitor(cv : ClassVisitor, visibilityFlags : Int, name : String, signature : String) extends MethodVisitor(ASM5) {
+  val methodVisitor = cv.visitMethod(visibilityFlags, name, signature, null, new Array[String](0))
+
+  def emitGetStatic(className : String, fieldName : String, fieldType : String): Unit = {
+    methodVisitor.visitFieldInsn(GETSTATIC, className, fieldName, fieldType)
+  }
+
+  def emitInvokeVirtual(ownerType : String, methodName : String, methodType : String, onInterface : Boolean): Unit = {
+    methodVisitor.visitMethodInsn(INVOKEVIRTUAL, ownerType, methodName, methodType, onInterface)
+  }
+
+  def emitReturn(): Unit = {
+    methodVisitor.visitInsn(RETURN)
+  }
+
+  override def visitMaxs(maxStack : Int, maxLocals : Int): Unit = {
+    methodVisitor.visitMaxs(maxStack, maxLocals)
+  }
+
+  override def visitInsn(opCode : Int): Unit = {
+    methodVisitor.visitInsn(opCode)
+  }
+
+  override def visitIntInsn(opCode : Int, arg : Int): Unit = {
+    methodVisitor.visitIntInsn(opCode, arg)
+  }
+
+  override def visitVarInsn(opCode : Int, arg : Int): Unit = {
+    methodVisitor.visitVarInsn(opCode, arg)
+  }
+
+  override def visitTypeInsn(opCode : Int, arg : String): Unit = {
+    methodVisitor.visitTypeInsn(opCode, arg)
+  }
+
+  override def visitLdcInsn(obj : AnyRef): Unit = {
+    methodVisitor.visitLdcInsn(obj)
+  }
+
+  override def visitEnd(): Unit = {
+    methodVisitor.visitEnd()
+  }
+
+  override def visitMethodInsn(opCode : Int, owner : String, methodName : String, methodDescriptor : String, onInterface : Boolean): Unit = {
+    methodVisitor.visitMethodInsn(opCode, owner, methodName, methodDescriptor, onInterface)
+  }
+
+  override def visitFieldInsn(opCode : Int, owner : String, fieldName : String, fieldDescriptor : String): Unit = {
+    methodVisitor.visitFieldInsn(opCode, owner, fieldName, fieldDescriptor)
+  }
+
+  override def visitAttribute(attr : Attribute): Unit = {
+    methodVisitor.visitAttribute(attr)
+  }
+
+  override def visitCode() : Unit = {
+    methodVisitor.visitCode()
+  }
+
+  override def visitInvokeDynamicInsn(name : String, desc : String, bsm : Handle, bsmArgs : Object*): Unit = {
+    methodVisitor.visitInvokeDynamicInsn(name, desc, bsm, bsmArgs:_*)
+  }
+
+  override def visitJumpInsn(opCode : Int, label : Label): Unit = {
+    methodVisitor.visitJumpInsn(opCode, label)
+  }
+
+  override def visitLabel(l : Label): Unit = {
+    methodVisitor.visitLabel(l)
+  }
+
+  override def visitIincInsn(opCode : Int, increment : Int): Unit = {
+    methodVisitor.visitIincInsn(opCode, increment)
+  }
+
+  override def visitTableSwitchInsn(min : Int, max : Int, default : Label, labels : Label*): Unit = {
+    methodVisitor.visitTableSwitchInsn(min, max, default, labels:_*)
+  }
+
+  override def visitLookupSwitchInsn(default : Label, keys : Array[Int], labels : Array[Label]): Unit = {
+    methodVisitor.visitLookupSwitchInsn(default, keys, labels)
+  }
+
+  override def visitMultiANewArrayInsn(desc : String, dims : Int): Unit = {
+    methodVisitor.visitMultiANewArrayInsn(desc, dims)
+  }
+
+  override def visitInsnAnnotation(typeRef : Int, typePath : TypePath, desc : String, visible : Boolean): AnnotationVisitor = {
+    methodVisitor.visitInsnAnnotation(typeRef, typePath, desc, visible)
+  }
+
+  override def visitTryCatchBlock(start : Label, end : Label, handler : Label, exceptionType : String): Unit = {
+    methodVisitor.visitTryCatchBlock(start, end, handler, exceptionType)
+  }
+
+  override def visitTryCatchAnnotation(typeRef : Int, typePath : TypePath, desc : String, visible : Boolean): AnnotationVisitor = {
+    methodVisitor.visitTryCatchAnnotation(typeRef, typePath, desc, visible)
+  }
+
+  override def visitLocalVariable(name : String, desc : String, signature : String, start : Label, end : Label, index : Int) = {
+    methodVisitor.visitLocalVariable(name, desc, signature, start, end, index)
+  }
+
+  override def visitLocalVariableAnnotation(typeRef : Int, typePath : TypePath, start : Array[Label], end : Array[Label], index : Array[Int], desc : String, visible : Boolean): AnnotationVisitor = {
+    methodVisitor.visitLocalVariableAnnotation(typeRef, typePath, start, end, index, desc, visible)
+  }
+
+  override def visitLineNumber(line : Int, start : Label): Unit = {
+    methodVisitor.visitLineNumber(line, start)
+  }
+
+  override def visitFrame(typeN : Int, nLocal : Int, local : Array[Object], nStack : Int, stack : Array[Object]): Unit = {
+    methodVisitor.visitFrame(typeN, nLocal, local, nStack, stack)
+  }
+}
